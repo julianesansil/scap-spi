ad922aaafdcfe10eb94cb0186a70f45f9b712230
Juliane Silva <jl.sansil@gmail.com>
2015-11-14 09:40:20 -0400

    Ajuste de nomes

--- src/spi/__init__.py
+++ src/spi/__init__.py
@@ -5,7 +5,7 @@ Created on 04/10/2015
 '''
 
 
-from scap.Preparador import Preparador
-from scap.Indexador import Indexador
-from scap.Buscador import Buscador
-from scap.Experimento import Experimento
+from spi.Preparador import Preparador
+from spi.Indexador import Indexador
+from spi.Buscador import Buscador
+from spi.Experimento import Experimento
